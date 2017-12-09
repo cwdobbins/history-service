f2a9c9a9acb3b1fb64d4c1c227146a8d33ec1cee
Christopher Dobbins <christopher.dobbins@culebrasoftware.com>
2017-12-09 13:19:23 -0500

    Added date/time parsing and nested object storage example

--- src/main/kotlin/HistoryDSL.kt
+++ src/main/kotlin/HistoryDSL.kt
@@ -1,30 +1,40 @@
+import java.util.Date
+import java.time.Instant
 import org.jetbrains.exposed.sql.*
 import org.joda.time.DateTime
+import java.text.SimpleDateFormat
 
 data class Search(
     val id: String,
     val name: String,
-    val language: String = "",
-    val submitted: String = ""
+    val submitted: Date = Date.from(Instant.now()),
+    val keywordSearch: KeywordSearch? = null
+)
+
+data class KeywordSearch(
+    val language: String,
+    val keyword: String
 )
 
 object Searches : Table() {
     val id = text("id").primaryKey()
     val userName = text("user_name").primaryKey()
-    val name = text("name").nullable()
-    val language = text("language").nullable()
-    val submitted = datetime("submitted").nullable()
+    val name = text("name")
+    val submitted = datetime("submitted")
+    val keywordLanguage = text("keyword_language").nullable()
+    val keywordKeyword = text("keyword_keyword").nullable()
 }
 
 class HistoryDSL(jdbcUrl: String, jdbcDriver: String) {
+    val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX")
     init {
         Database.connect(jdbcUrl, jdbcDriver)
     }
 
     fun get(user: String, id: String): Search {
-        Searches.select {
+        println(Searches.select {
             (Searches.id.eq(id) and Searches.userName.eq(user))
-        }
+        })
         return Search("1", "test")
     }
 
@@ -33,8 +43,11 @@ class HistoryDSL(jdbcUrl: String, jdbcDriver: String) {
             it[id] = search.id
             it[userName] = user
             it[name] = search.name
-            it[language] = search.language
-            //it[submitted] = DateTime.parse(search.submitted)
+            it[submitted] = DateTime.parse(dateFormat.format(search.submitted))
+            search.keywordSearch?.let { kw ->
+                it[keywordLanguage] = kw.language
+                it[keywordKeyword] = kw.keyword
+            }
         }
     }
 }
